f01f20883a7c495fd299f6405bc68111b452203a
Paulo Ricardo <paulo.tolkien@outlook.com>
2024-02-06 00:29:18 -0300

    finalizando roteamento e testes log

--- Routing/EmitLogDirect/EmitLogDirect.cs
+++ Routing/EmitLogDirect/EmitLogDirect.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace EmitLogDirect
 {
@@ -6,7 +8,32 @@ namespace EmitLogDirect
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:5672/") };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("direct_logs", ExchangeType.Direct);
+            var gravidade = (args.Length > 0) ? args[0] : "informações";
+
+            string message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange: "direct_logs",
+                                 routingKey: gravidade,
+                                 basicProperties: null,
+                                 body: body);
+
+            Console.WriteLine("Message sent. Press any button to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello world!");
         }
     }
 }
+
--- Routing/ReceiveLogsDirect/ReceiveLogsDirect.cs
+++ Routing/ReceiveLogsDirect/ReceiveLogsDirect.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace ReceiveLogsDirect
 {
@@ -6,7 +11,53 @@ namespace ReceiveLogsDirect
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:5672/") };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            // channel.QueueDeclare(queue: "emit_log_queue",
+            //                      durable: true,
+            //                      exclusive: false,
+            //                      autoDelete: false,
+            //                      arguments: null);
+
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            foreach (var gravidade in args)
+            {
+                channel.QueueBind(queue: queueName,
+                                  exchange: "direct_logs",
+                                  routingKey: gravidade);
+            }
+
+            // Fair Dispatch
+            // This configuration tells to RabbitMQ don't dispatch a new message 
+            // to a worker until it has processed and acknowledged the previous one.
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            Console.WriteLine("[*] Waiting for messages!");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Have one Task: {message}");
+                int points = message.Split(" ").Length - 1;
+                Thread.Sleep(points * 100);
+
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine("Press any button to exit.");
+            Console.ReadLine();
         }
     }
 }
